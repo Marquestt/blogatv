84a80b4d4a8386f6778bbd02275ce0489d709870
Thatyane Marques <111459584+Marquestt@users.noreply.github.com>
2024-11-18 10:57:38 -0300

    config render

---
+++ src/main/java/com/blogbd/blogatv/BlogatvApplication.java
@@ -0,0 +1,13 @@
+package com.blogbd.blogatv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogatvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogatvApplication.class, args);
+	}
+
+}
